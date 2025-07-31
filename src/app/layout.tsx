aec31544061546f99cacff159158b7e254d76131
overstarry <jm1179356055@gmail.com>
2025-07-31 20:02:03 +0800

    add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R70PSR49J8"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-R70PSR49J8');
+            `,
+          }}
+        />
+      </head>
       <body className="antialiased">
         <ThemeProvider defaultTheme="system">
           {children}
